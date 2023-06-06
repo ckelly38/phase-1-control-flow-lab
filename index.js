83bfa681157bf34cc6c2bb910b02505eab660dcc
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-05 22:55:42 -0600

    flow control and ternary shorthand if else (cond ? true : false) means if cond is true returns true else : it returns false and switchs and if else if else statements

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ridelen){
+  if (ridelen < 400 || ridelen == 400)
+  {
+    return "This one is on me!";
+  }
+  else if (ridelen > 400 && (ridelen < 2000 || ridelen == 2000))
+  {
+    return "That will be twenty bucks.";
+  }
+  else if (ridelen > 2000 && ridelen < 2500)
+  {
+    return "I will gladly take your thirty bucks.";
+  }
+  else
+  {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(true)
+  {
+    case tip === "generous":
+      return "Thank you so much.";
+    case tip === "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
